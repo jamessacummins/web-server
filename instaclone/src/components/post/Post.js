6ea7c9abf8032a1e8a585abeba29182fea1b02ce
James Cummins <james.cummins@renewtrak.com>
2021-12-31 15:58:19 +1100

    adding mobile responsiveness

--- instaclone/src/components/post/Post.js
+++ instaclone/src/components/post/Post.js
@@ -1,9 +1,14 @@
 import './Post.css';
 
+const dataLocation = process.env.PUBLIC_URL + "/data";
+
 function Post() {
     return(
         <div className="Post">
-            
+            <img 
+                className="Post-image"
+                src={ dataLocation + "/1/image.jpg" } 
+            />
         </div>
     );
 }
