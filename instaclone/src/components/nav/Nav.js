d4198f6eb5005087752b76f33cdea03cb486cd3a
James Cummins <james.cummins@renewtrak.com>
2022-01-01 10:46:47 +1100

    updating feed

--- instaclone/src/components/nav/Nav.js
+++ instaclone/src/components/nav/Nav.js
@@ -4,16 +4,19 @@ import homeIcon from '../../assets/home.png';
 import findIcon from '../../assets/find.png';
 import messagesIcon from '../../assets/messages.png';
 import notifictionsIcon from '../../assets/notifications.png';
+import searchIcon from '../../assets/search.svg';
+import profileIcon from '../../assets/profile.jpg';
 
 function Nav() {
     return (
       <div id="Nav-container">
         <div id="Nav-wrapper">
             <div id="Nav-logo-wrapper" className='growable-child'>
-                <img src={logo} />
+                <img id="Nav-logo" src={logo} />
             </div>
             <div id="Nav-search-wrapper" className='growable-child'>
                 <div id="Nav-search-box">
+                    <img id="Nav-search-icon" src={searchIcon} />
                     Search
                 </div>
             </div>
@@ -22,6 +25,7 @@ function Nav() {
                 <img src={messagesIcon} className='icon'/>
                 <img src={findIcon} className='icon'/>
                 <img src={notifictionsIcon} className='icon'/>
+                <img src={profileIcon} id="Nav-profile-icon"  className="icon" />
             </div>
         </div>
       </div>
--- instaclone/src/components/post/Post.js
+++ instaclone/src/components/post/Post.js
@@ -1,14 +1,61 @@
 import './Post.css';
+import detailsIcon from '../../assets/details.png';
+import likeIcon from '../../assets/like.png';
+import commentIcon from '../../assets/comment.png';
+import saveIcon from '../../assets/save.png';
+import forwardIcon from '../../assets/forward.png';
 
 const dataLocation = process.env.PUBLIC_URL + "/data";
 
 function Post() {
     return(
         <div className="Post">
+            <div className="Post-header-wrapper">
+                
+                    <img 
+                        className="Post-header-profile-pic" 
+                        src={ dataLocation + "/1/profile.jpg" }
+                    />
+                    <div className="Post-header-profile-name">
+                        jamesclear
+                    </div>
+                    <img 
+                        className="Post-header-details"
+                        src={detailsIcon} 
+                    />
+                
+            </div>
+            
             <img 
                 className="Post-image"
                 src={ dataLocation + "/1/image.jpg" } 
             />
+            <div className="Post-actions-wrapper">
+                <img 
+                    className="Post-actions-like Post-action"
+                    src={likeIcon} 
+                />
+                <img 
+                    className="Post-actions-comment Post-action"
+                    src={commentIcon} 
+                />
+                <img 
+                    className="Post-actions-forward Post-action"
+                    src={forwardIcon} 
+                />
+                <img 
+                    className="Post-actions-save Post-action"
+                    src={saveIcon} 
+                />
+            </div>
+            <div className="Post-caption-wrapper">
+                <span className="Post-caption-user">
+                    jamesclear
+                </span>
+                <span className="Post-caption-text">
+                    Sign up to my free course now at jamesclear.com. 15% off with code VOTEFORCHANGE.
+                </span>
+            </div>
         </div>
     );
 }
