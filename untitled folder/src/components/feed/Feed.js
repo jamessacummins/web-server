d0e6af84154b198014ade63168a7094fd0c20478
James Cummins <james.cummins@renewtrak.com>
2021-12-31 14:04:38 +1100

    testing

---
+++ untitled folder/src/components/feed/Feed.js
@@ -0,0 +1,17 @@
+import './Feed.css';
+
+import Post from '../post/Post';
+
+function Feed() {
+    return(
+        <div id="Feed">
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+        </div>
+    );
+}
+
+export default Feed;
