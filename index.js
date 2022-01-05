4665881112158c5143dabb206acc9ab44507b6dc
James Cummins <james.cummins@renewtrak.com>
2022-01-05 11:47:27 +1100

    adding mysql to index

--- index.js
+++ index.js
@@ -17,4 +17,21 @@ app.use('/instaclone', express.static('instaclone/build', {
     extensions: ['html', 'htm'],
 }) );
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'pa123pa123'
+});
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
+
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
